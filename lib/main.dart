827f86b841277a0e1c73da6fa5bceea41de40c28
Gleydson dias Ferreira <gleydson_developer@outlook.com>
2023-10-02 21:47:07 -0300

    commit-03
    Adicionando minha Drawer, meu Appbar e modificando o layout do app

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,64 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar:  AppBar(
+          backgroundColor: Colors.black,
+          elevation: 0,
+          // Adicione o ícone do menu do drawer
+          leading: Builder(
+            builder: (BuildContext context) {
+              return IconButton(
+                icon: const Icon(Icons.menu),
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
+              );
+            },
+          ),
+          title: const Text('My Awesome App'),
+        ),
+        // Adicione o drawer
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                ),
+                child: Text(
+                  'Menu',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              ListTile(
+                title: const Text('Opção 1'),
+                onTap: () {
+                  // Adicione o código para a ação da opção 1
+                },
+              ),
+              ListTile(
+                title: const Text('Opção 2'),
+                onTap: () {
+                  // Adicione o código para a ação da opção 2
+                },
+              ),
+              // Adicione mais opções do drawer, se necessário
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
